42726919cc4d65e317dada3b738964b4b1ad1d1d
hoodsey20 <hoodsey20@gmail.com>
2019-04-03 15:43:10 +0700

    добавил нормальный hot-reload и импорт изображений из html-файлов

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,90 @@
 const path = require(`path`);
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: `production`,
-  entry: `./src/index.js`,
-  output: {
-    filename: `[name].js`,
-    path: path.join(__dirname, `public`)
-  },
-  optimization: {
-    splitChunks: {
-      name: 'vendor',
-      chunks: 'all',
+module.exports = (env, options) => {
+  const isProd = options.mode === 'production';
+
+  const finalCssLoader = isProd ? { loader: MiniCssExtractPlugin.loader} : 'style-loader'
+
+  const styleLoaders = [
+    finalCssLoader,
+    'css-loader',
+    'sass-loader',
+  ];
+
+  const config = {
+    entry: `./src/index.js`,
+    output: {
+      filename: `[name].js`,
+      publicPath: '',
+      path: path.join(__dirname, `public`)
     },
-  },
-  plugins: [
-    ...['index','catalog'].map((event) => {
-      return new HtmlWebpackPlugin({
-        template: `./src/${event}.html`,
-        filename: `${event}.html`,
-      })
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    })
-  ],
-  devtool: `source-map`,
-  devServer: {
-    contentBase: path.join(__dirname, `public`),
-    publicPath: `http://localhost:8080`,
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
+    optimization: {
+      splitChunks: {
+        name: 'vendor',
+        chunks: 'all',
       },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          'sass-loader',
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            name: '[name].[ext]',
-            limit:150,  //it's important
-            outputPath: 'img',
-            publicPath: '../img',
+    },
+    plugins: [
+      ...['index','catalog'].map((event) => {
+        return new HtmlWebpackPlugin({
+          template: `./src/${event}.html`,
+          filename: `${event}.html`,
+        })
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+      }),
+    ],
+    devtool: `source-map`,
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      watchContentBase: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: styleLoaders 
+        },
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              attrs: [':src', ':data-src']
+            }
+          }
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]',
+              limit: 9192,  //it's important
+              outputPath: 'img',
+              publicPath: '../img',
+              mimetype: 'image/jpg',
+            }
           }
         }
-      },
-    ]
+      ]
+    }
   }
+
+  if (!isProd) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+  
+  return config;
 };
